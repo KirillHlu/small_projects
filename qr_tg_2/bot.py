b892595fd0a163a969b6e5cbe7f5add13c120b62
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-07-15 18:02:33 +0700

    Update bot.py

--- qr_tg_2/bot.py
+++ qr_tg_2/bot.py
@@ -1,72 +1,115 @@
 import telebot
 from telebot import types
 import json
+import flet as ft
+import threading
 
-TOKEN = 'TOKEN'
-bot = telebot.TeleBot(TOKEN)
+def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, 'Hello, this is your bot for qr-code! \n\n Send /reg "password" for create account.', reply_markup=webAppKeyboard())
+    bot = None
+    bot_thread = None
 
-def webAppKeyboard():  
-    keyboard = types.ReplyKeyboardMarkup(row_width=1)  
-    webAppTest = types.WebAppInfo(
-        "SITE")  
-    one_butt = types.KeyboardButton(text="Open app", web_app=webAppTest)  
-    keyboard.add(one_butt) 
-    return keyboard
+    def bot_def(e):
+        page.snack_bar = ft.SnackBar(ft.Text("Бот запустился!", color=ft.colors.LIGHT_BLUE_ACCENT_700), bgcolor=ft.colors.BLACK54)
+        nonlocal bot, bot_thread
+        TOKEN = token_input.value
+        bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['reg'])
-def send_welcome(message):
-    filename = 'users.json'
-    data = []
+        # Удаление вебхука перед началом опроса
+        bot.remove_webhook()
 
-    try:
-        with open(filename, 'r') as file:
-            data = json.load(file)
-            if not isinstance(data, list):
+        @bot.message_handler(commands=['start'])
+        def send_welcome(message):
+            bot.send_message(message.chat.id, 'Здравствуйте, это бот для создания QR-кодов! \n\nОтправьте /reg "имя" "пароль" для регистрации аккаунта.\n\nЕсли у вас появились проблемы - /help', reply_markup=webAppKeyboard())
+
+        def webAppKeyboard():
+            keyboard = types.ReplyKeyboardMarkup(row_width=1)
+            webAppTest = types.WebAppInfo(url_input.value)
+            one_butt = types.KeyboardButton(text="Открыть приложение", web_app=webAppTest)
+            keyboard.add(one_butt)
+            return keyboard
+
+        @bot.message_handler(commands=['reg'])
+        def register_user(message):
+            try:
+                filename = 'users.json'
                 data = []
-    except FileNotFoundError:
-        pass
 
-    name = message.text.split()[1]
-    password = message.text.split()[2]
-    chat_id = message.chat.id
+                try:
+                    with open(filename, 'r') as file:
+                        data = json.load(file)
+                        if not isinstance(data, list):
+                            data = []
+                except FileNotFoundError:
+                    pass
 
-    answer = 'Yes'
+                name = message.text.split()[1]
+                password = message.text.split()[2]
+                chat_id = message.chat.id
 
-    for user in data:
-        if user.get('chat_id') == chat_id:
-            answer = 'No'
+                answer = 'Yes'
 
-    for user in data:
-        if user.get('name') == name:
-            answer = 'No2'
+                for user in data:
+                    if user.get('chat_id') == chat_id:
+                        answer = 'No'
 
-    if answer == 'No':
-        bot.send_message(message.chat.id, 'You have an account!')
-        for user in data:
-            if user.get('chat_id') == chat_id:
-                bot.send_message(message.chat.id, f'Your data:\nName: {user.get("name")}\nPassword: {user.get("password")}')
+                for user in data:
+                    if user.get('name') == name:
+                        answer = 'No2'
 
-    elif answer == 'No2':
-        bot.reply_to(message, 'This name is busy!')
+                if answer == 'No':
+                    bot.send_message(message.chat.id, 'У вас есть аккаунт!')
+                    for user in data:
+                        if user.get('chat_id') == chat_id:
+                            bot.send_message(message.chat.id, f'Ваша информация:\nИмя: {user.get("name")}\nПароль: {user.get("password")}')
 
-    else:
-        data.append({
-            "name": name,
-            "password": password,
-            "chat_id": chat_id
-        })
-        bot.reply_to(message, 'Successful!')
+                elif answer == 'No2':
+                    bot.reply_to(message, 'Такое имя занято!')
 
+                else:
+                    data.append({
+                        "name": name,
+                        "password": password,
+                        "chat_id": chat_id
+                    })
+                    bot.reply_to(message, 'Сохранено!')
 
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
+                with open(filename, 'w') as file:
+                    json.dump(data, file, indent=4)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "Sorry, i don't understand!")
+            except:
+                bot.reply_to(message, 'Отправьте вашу информацию правильно!')
 
-bot.polling()
+        @bot.message_handler(commands=['help'])
+        def send_help(message):
+            bot.send_message(message.chat.id, f'Если у вас возникли проблемы с приложением, то посетите сайт {url_input.value}.')
+
+        @bot.message_handler(func=lambda message: True)
+        def echo_all(message):
+            bot.reply_to(message, "Простите, я не понимаю!")
+
+        bot_thread = threading.Thread(target=bot.polling)
+        bot_thread.start()
+
+    def stop_bot(e):
+        nonlocal bot, bot_thread
+        if bot:
+            bot.stop_polling()
+            bot_thread.join()
+            bot = None
+            bot_thread = None
+            page.snack_bar = ft.SnackBar(ft.Text("Бот остановлен!", color=ft.colors.LIGHT_BLUE_ACCENT_700), bgcolor=ft.colors.BLACK54)
+            page.snack_bar.open = True
+            page.update()
+
+    token_input = ft.TextField(label='Токен')
+    url_input = ft.TextField(label='Ссылка')
+    start_btn = ft.ElevatedButton(text='Старт', on_click=bot_def)
+    stop_btn = ft.ElevatedButton(text='Остановить бота', on_click=stop_bot)
+
+    page.add(token_input, url_input, ft.Row([start_btn, stop_btn],alignment=ft.MainAxisAlignment.CENTER))
+
+if __name__ == '__main__':
+    ft.app(target=main)
