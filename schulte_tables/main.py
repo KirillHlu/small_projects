ece43cfc0349c24a28f1dfda7871290806203066
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-07-23 02:54:29 +0700

    Create main.py

---
+++ schulte_tables/main.py
@@ -0,0 +1,38 @@
+import flet as ft
+import random
+
+def main(page: ft.Page):
+    page.scroll = True
+    page.add(ft.Text(" ", size=100))
+
+    for i in range(10):
+        numbers = []
+        for j in range(5):
+            row = ft.Row(controls=[], alignment=ft.MainAxisAlignment.CENTER)
+            for h in range(5):
+                cnt = 0
+                while cnt == 0:
+                    num = random.randint(1,25)
+                    if num in numbers:
+                        pass
+                    else:
+                        numbers.append(num)
+                        cnt = 1
+                        row.controls.append(
+                            ft.Container(
+                                content=ft.Text(f"{num}", color=ft.colors.BLACK54),
+                                bgcolor=ft.colors.WHITE70,
+                                border_radius=0.1,
+                                width=50,
+                                height=50,
+                                alignment=ft.alignment.center
+                            )
+                        )
+            page.add(row)
+        page.add(ft.Text(" ", size=100))
+
+        print(numbers)
+
+
+
+ft.app(target=main)
