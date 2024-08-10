374ecbc2d73f85078e15b012c00706844bfcb1ae
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-08-10 15:31:34 +0700

    Update first_img.py

--- cv2/first_img.py
+++ cv2/first_img.py
@@ -1,6 +1,21 @@
 import cv2
+import numpy as np
 
-img = cv2.imread('images/image_100820242.jpg')
-cv2.imshow('Hello, world!', img)
+img = cv2.imread('images/image_10082024.jpg')
+
+img = cv2.resize(img, (800,500))
+# img = cv2.GaussianBlur(img,(111,111), 0)        #размытие
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)       #цвет
+
+img = cv2.Canny(img, 200, 200)                    #для контуров
+
+kernel = np.ones((5,5), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+
+img = cv2.erode(img, kernel, iterations=1)
+
+cv2.imshow('Hello, world!',img)
+
+print(img.shape)
 
 cv2.waitKey(0)
