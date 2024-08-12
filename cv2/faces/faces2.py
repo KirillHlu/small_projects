2254b0ac9c3d1db037a6e461cbac6c04168880f5
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-08-13 02:15:44 +0700

    Update faces2.py

--- cv2/faces/faces2.py
+++ cv2/faces/faces2.py
@@ -17,10 +17,9 @@ while True:
 
     for (x, y, w, h) in results:
         cv2.rectangle(img_resized, (x, y), (x + w, y + h), (0, 255, 0), thickness=3)
+        cv2.putText(img_resized, 'Face', (x, y-10), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 0), 1)
 
     cv2.imshow('Face Detection', img_resized)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
-
-
