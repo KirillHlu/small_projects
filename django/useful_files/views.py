eb868d840e3505f308713a3124dd0ad39308eb56
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-08-20 00:08:40 +0700

    Update views.py

--- django/useful_files/views.py
+++ django/useful_files/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Articles
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Articles.objects.all()
     return render(request, 'main/index.html', {'news': news})
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'main/details_view.html'
+    context_object_name = 'article'
 
 def index(request):
     return render(request, 'main/index.html')
