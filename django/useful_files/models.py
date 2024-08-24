e12856f6abb5e7b814211726b552ab2d8708151a
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-08-24 12:16:54 +0700

    Update models.py

--- django/useful_files/models.py
+++ django/useful_files/models.py
@@ -1,15 +1,19 @@
+# myapp/models.py
+from django.contrib.auth.models import User
 from django.db import models
-
 
 class Articles(models.Model):
     title = models.CharField('Title', max_length=50)
     anons = models.CharField('Anons', max_length=250)
     full_text = models.TextField('Text')
-    date = models.DateField('Date')
+    date = models.DateField('Date', auto_now_add=True)  # Это поле автозаполняется при создании
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'News'
-        verbose_name_plural = 'News'
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+
