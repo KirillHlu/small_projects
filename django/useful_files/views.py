7e1cdebdc0852edbf475e71235585216fe7b186b
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-08-24 12:10:40 +0700

    Update views.py

--- django/useful_files/views.py
+++ django/useful_files/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Articles
 from django.views.generic import DetailView
+from django.shortcuts import render, redirect
+from .forms import ArticlesForm
+from django.contrib.auth.decorators import login_required
+
 
 def news_home(request):
     news = Articles.objects.all()
-    return render(request, 'main/index.html', {'news': news})
+    return render(request, 'main/index.html', {'news': list(reversed(news))})
 
 class NewsDetailView(DetailView):
     model = Articles
@@ -15,5 +17,15 @@ class NewsDetailView(DetailView):
 def index(request):
     return render(request, 'main/index.html')
 
-def about(request):
-    return HttpResponse('<h4>I will do it later</h4>')
+@login_required
+def create_article(request):
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user  # Привязываем статью к текущему пользователю
+            article.save()  # Сохраняем статью
+            return redirect('news-home')  # Перенаправление на список статей
+    else:
+        form = ArticlesForm()
+    return render(request, 'main/create_article.html', {'form': form})
