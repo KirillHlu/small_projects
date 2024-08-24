d35100432554d27c7b857b8ceeca97af65246c49
KirillHlu <164990613+KirillHlu@users.noreply.github.com>
2024-08-24 12:10:11 +0700

    Update second_urls.py

--- django/useful_files/second_urls.py
+++ django/useful_files/second_urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.news_home, name='news-home'), 
-    path('about/', views.about, name='about'),    
-    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail')
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('create_article/', views.create_article, name='create_article'),
+    path('accounts/profile/', views.news_home, name='news-home'),
 ]
